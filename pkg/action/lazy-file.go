876a645a8181b30f7d0c4171a598ff4994bf2f4a
Anatoly Myachev <anatoly.myachev@intel.com>
2020-01-10 18:09:18 +0300

    add TestLazyFile; add name for lazyFile struct

--- pkg/action/lazy-file.go
+++ pkg/action/lazy-file.go
@@ -7,17 +7,19 @@ import (
 
 type lazyFile struct {
 	out    io.WriteCloser
+	name   string
 	prefix string
 }
 
 func (lw *lazyFile) Write(p []byte) (int, error) {
 	if lw.out == nil {
-		_, out, err := makeLogWriter(lw.prefix)
+		fileName, out, err := makeLogWriter(lw.prefix)
 		if err != nil {
 			return 0, err
 		}
 
 		lw.out = out
+		lw.name = fileName
 	}
 
 	n, err := lw.out.Write(p)
