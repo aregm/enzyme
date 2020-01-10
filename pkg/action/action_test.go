da0980ace282949730d5139b9c9b0bf781a41ce5
anmyachev <45976948+anmyachev@users.noreply.github.com>
2020-01-10 18:25:42 +0300

    add TestLazyFile; add name for lazyFile struct (#20)
    * add TestLazyFile; add name for lazyFile struct
    * initialization lazyFile vars without fields with default value

--- pkg/action/action_test.go
+++ pkg/action/action_test.go
@@ -28,7 +28,6 @@ func TestMakeLogWriter(t *testing.T) {
 	}
 
 	readContent, err := ioutil.ReadFile(logName)
-
 	if err != nil {
 		t.Errorf("error occurred while trying to read from the log file: [%s]", err)
 	}
@@ -42,5 +41,49 @@ func TestMakeLogWriter(t *testing.T) {
 
 	if err = os.Remove(logName); err != nil {
 		t.Errorf("error deleting file; file: [%s], error: [%s]", logName, err)
+	}
+}
+
+func TestLazyFile(t *testing.T) {
+	testFileNamePrefix := filepath.Join(os.TempDir(), "TestLazyFile")
+	testFileContent := []byte("test lazy file content")
+
+	lzFile := &lazyFile{prefix: testFileNamePrefix}
+
+	writtenBytes, err := lzFile.Write(testFileContent)
+	if err != nil {
+		t.Errorf("error occurred while trying to write to the lazy file: [%s]", err)
+	}
+	defer lzFile.Close()
+
+	// after first Write call, out should be not nil
+	if lzFile.out == nil {
+		t.Errorf("something went wrong")
+	}
+
+	if writtenBytes != len(testFileContent) {
+		t.Errorf("unexpected written bytes count: %d, expected: %d", writtenBytes, len(testFileContent))
+	}
+
+	readContent, err := ioutil.ReadFile(lzFile.name)
+	if err != nil {
+		t.Errorf("error occurred while trying to read from the lazy file: [%s]", err)
+	}
+
+	if equal := reflect.DeepEqual(readContent, testFileContent); equal != true {
+		t.Errorf("recorded content does not match read content")
+	}
+
+	// still need to close explicitly instead of relying on defer so os.Remove succeeds
+	lzFile.Close()
+
+	// after call Close method, Write method should return error != nil
+	_, err = lzFile.Write(testFileContent)
+	if err == nil {
+		t.Errorf("Close method does not actually close the file")
+	}
+
+	if err = os.Remove(lzFile.name); err != nil {
+		t.Errorf("error deleting file; file: [%s], error: [%s]", lzFile.name, err)
 	}
 }
