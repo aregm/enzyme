a7a844e2a8540dd7385efdbab245f3ff1b696a27
Anatoly Myachev <anatoly.myachev@intel.com>
2020-01-14 02:16:19 +0300

    clck, mpi and mkl installation via yum

--- distrib/intel_tools_install.sh
+++ distrib/intel_tools_install.sh
@@ -2,34 +2,13 @@
 
 set -e
 
-PRODUCT_DIR="$1"
-PRODUCT_PACKAGE="$2"
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://yum.repos.intel.com/clck/2019/setup/intel-clck-2019.repo
+sudo yum-config-manager --add-repo https://yum.repos.intel.com/mpi/setup/intel-mpi.repo
+sudo yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
 
-# check if product package is already installed
-rpm -qa | grep -q "${PRODUCT_PACKAGE}" && exit 0 || true
+sudo rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 
-cd "`dirname $0`/${PRODUCT_DIR}"
-
-DISTR_FILE=`ls *.tgz`
-DISTR_NAME=`echo $DISTR_FILE | sed 's/\.[^.]*$//'`
-
-if [ ! -f ./$DISTR_NAME/install.sh ]; then
-    tar xvf $DISTR_FILE
-fi
-
-cp silent.cfg silent-fixed.cfg
-dos2unix silent-fixed.cfg
-
-if grep -qE '^ACTIVATION_TYPE=license_file' silent-fixed.cfg ; then
-    LICENSE_FILE=`ls *.lic`
-    if [ -z "$LICENSE_FILE" ]; then
-        echo "License file needed for $1 but is not uploaded"
-        exit 1
-    fi
-    echo "ACTIVATION_LICENSE_FILE=`pwd`/$LICENSE_FILE" >>silent-fixed.cfg
-fi
-
-sudo ./$DISTR_NAME/install.sh --silent `pwd`/silent-fixed.cfg
-rm -f silent-fixed.cfg
-
-echo "$1 installed successfully"
+sudo yum install -y intel-clck-2019.6-038
+sudo yum install -y intel-mpi-2019.5-075
+sudo yum install -y intel-mkl-2019.5-075
