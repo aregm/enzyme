3b1610ad951c0c26f5b13f1021d93b97f4ef42c4
Anatoly Myachev <anatoly.myachev@intel.com>
2020-01-15 18:43:25 +0300

    add TestRunLoggedCmdDirOutput

--- pkg/action/action_test.go
+++ pkg/action/action_test.go
@@ -1,10 +1,12 @@
 package action
 
 import (
+	"bytes"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -86,4 +88,44 @@ func TestLazyFile(t *testing.T) {
 	if err = os.Remove(lzFile.name); err != nil {
 		t.Errorf("error deleting file; file: [%s], error: [%s]", lzFile.name, err)
 	}
+}
+
+func TestRunLoggedCmdDirOutput(t *testing.T) {
+	testLogFilePrefix := filepath.Join(os.TempDir(), "TestRunLoggedCmdDirOutput")
+	var buffer bytes.Buffer
+	workdir := ""
+	name := "go"
+	arg := "version"
+
+	logName, err := RunLoggedCmdDirOutput(testLogFilePrefix, workdir, &buffer, name, arg)
+	if err != nil {
+		t.Errorf("error occurred while trying to run command: [%s %s], error: [%s]", name, arg, err)
+	}
+
+	content := buffer.String()
+	expectedBufferContent := "go version go1.13.3 windows/amd64"
+	expectedLogFileContent := "Rhoc: running command: go version"
+
+	if strings.HasPrefix(content, expectedBufferContent) != true {
+		t.Errorf("recorded content: [%s] does not match read content: [%s]", content, expectedBufferContent)
+	}
+
+	logContent, err := ioutil.ReadFile(logName)
+	if err != nil {
+		t.Errorf("error occurred while trying to read from the log file: [%s]", err)
+	}
+
+	if strings.HasPrefix(string(logContent), expectedLogFileContent) != true {
+		t.Errorf("recorded content: [%s] does not match read content: [%s]", content, expectedBufferContent)
+	}
+
+	logName2, err := RunLoggedCmd(testLogFilePrefix, name, arg)
+	if err != nil {
+		t.Errorf("error occurred while trying to run command: [%s %s], error: [%s]", name, arg, err)
+	}
+
+	if logName != logName2 {
+		t.Errorf("something went wrong")
+	}
+
 }
